79506eb9a6afc198a39afd2b5e207081fb25c387
modernfuthark <53625935+modernfuthark@users.noreply.github.com>
2020-12-23 21:04:29 -0500

    [^] Task 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20;
 
+    private int score = 0;
+
     private Rigidbody player;
 
     // Start is called before the first frame update
@@ -40,6 +42,17 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             player.AddForce(speed * -1, 0, 0);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            Debug.Log($"Score: {score}");
+
+            Destroy(other.gameObject);
         }
     }
 }
